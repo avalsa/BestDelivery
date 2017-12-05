95cb868c7f849d33c2027fca87558cf0e24e5572
Unknown <BegunMD@yandex.ru>
2017-12-05 10:35:20 +0300

    created new fields in searchDeliveryForm, get and set  for every of them

--- src/main/java/com/besteam/bestapp/form/SearchDeliveryForm.java
+++ src/main/java/com/besteam/bestapp/form/SearchDeliveryForm.java
@@ -1,11 +1,32 @@
 package com.besteam.bestapp.form;
 
 
+import javax.validation.constraints.NotNull;
 
 public class SearchDeliveryForm {
+    //@NotNull
     private String from;
+    //инждекс откуда куда - по возможности скачивать автоматческ икак на сайте почты рф
+    //откуда город = подсказаьки,
+    //куда город
+    //вес граммы валидация
+    //страна, регион, город - из почты россии стянуть полученеие данных
+    //добавить валидацию
+    //если не получилось сделать подсказки, сделать чтобы и индекс вводился вручную
+    //если не получилось добавить поля для полного адреса страна край район город
 
+    //@NotNull
     private String to;
+    //@NotNull
+    private String date;
+
+    private String way;
+    //@NotNull
+    private String weight;
+
+    private boolean filter;
+
+    private boolean storyFilter;
 
     public String getTo() {
         return to;
@@ -21,5 +42,45 @@ public class SearchDeliveryForm {
 
     public void setFrom(String from) {
         this.from = from;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getWay() {
+        return way;
+    }
+
+    public void setWay(String way) {
+        this.way = way;
+    }
+
+    public boolean isFilter() {
+        return filter;
+    }
+
+    public void setFilter(boolean filter) {
+        this.filter = filter;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public boolean isStoryFilter() {
+        return storyFilter;
+    }
+
+    public void setStoryFilter(boolean storyFilter) {
+        this.storyFilter = storyFilter;
     }
 }
